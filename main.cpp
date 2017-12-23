0381339ec9b65b37ca02f518ab3dbf92adcbac2f
Aryan <aryan1998.tmwa@gmail.com>
2017-12-23 02:12:31 -0800

    new change

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aryanTest
+//
+//  Created by Aryan Arora on 2017-12-23.
+//  Copyright © 2017 Aryan Arora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
